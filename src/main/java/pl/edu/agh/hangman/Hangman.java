3cecb03559caaaefede9dde04ca22bee9fbbb3da
pawjamro <jamro.pawel@gmail.com>
2024-02-04 11:00:19 +0100

    Functionality:Load data from Text file into array

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.hangman;
 
+import java.io.FileNotFoundException;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -54,6 +56,10 @@ public class Hangman {
                     "========"
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(HANGMANPICS.length);
+        DataLoader test = new DataLoader();
+       // test.loadWords("src/main/resources/slowa.txt");
+        //System.out.println(test.getWords());
     }
 }
